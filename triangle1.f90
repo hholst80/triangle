6614dc41ea1eb5e6a0245802ad5e462d3aa8f6ef
Henrik Holst <holst@matmech.com>
2015-11-04 15:16:45 +0100

    Updated programs to read from stdin.

--- triangle1.f90
+++ triangle1.f90
@@ -10,31 +10,19 @@ program triangle_brute_force
   print*, '*************************************************************'
   print*,
 
-  if (iargc() == 0) then
-    print*, 'Please supply a data file as first argument'
-    return
-  end if
-
   height = 0
   triangle(:,:) = 0
 
   ! Read data
 
-  call getarg(1,filename)
-  open (1, file=filename, iostat=ires, status='old')
-  if (ires /= 0) then
-    print*, 'Failed to open file'
-    return
-  end if
   do
     height = height + 1
-    read (1, *, iostat=ires) triangle(height,1:height)
+    read (*, *, iostat=ires) triangle(height,1:height)
     if (ires /= 0) then
       height = height - 1
       exit
     end if
   end do
-  close (1)
 
   ! Compute cost
 
